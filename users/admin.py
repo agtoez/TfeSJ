87b03f6a01e15c8e30a1b961ec3a5eb3cd665d46
agtoez <agtoez@gmail.com>
2026-03-04 03:41:15 -0300

    Usuario administrador creado.

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+# Personalizamos cómo se ve el usuario en el panel
+class CustomUserAdmin(UserAdmin):
+    model = User
+    # Agregamos nuestros campos personalizados a la vista del Admin
+    fieldsets = UserAdmin.fieldsets + (
+        ('Información de Verificación', {'fields': ('role', 'phone', 'is_verified', 'verification_status', 'dni_front', 'dni_back', 'selfie')}),
+        ('Reputación', {'fields': ('rating_average', 'rating_count')}),
+    )
+
+admin.site.register(User, CustomUserAdmin)
